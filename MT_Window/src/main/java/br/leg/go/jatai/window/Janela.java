a117a0cb282981ae3b1618bca89f50e5e2a118d7
info <info@info-All-Series>
2024-08-29 10:20:54 -0300

    Segunda Atualização: Adição de uma text area e começo da implementação do Action Listener

--- MT_Window/src/main/java/br/leg/go/jatai/window/Janela.java
+++ MT_Window/src/main/java/br/leg/go/jatai/window/Janela.java
@@ -4,15 +4,19 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-public class Janela extends JFrame{
+public class Janela extends JFrame implements ActionListener{
     //Atributos
-        private JPanel pnDados, pnBotoes;
+        private JPanel pnDados, pnBotoes, pnToken;
+        private JTextArea taToken;
         private JTextField tfUser, tfSenha, tfUrl;
         private JLabel lbUser, lbSenha, lbUrl;
         private JButton btLogin;
@@ -25,10 +29,12 @@ public class Janela extends JFrame{
 
      //Configuração dos painéis
         pnDados = new JPanel();
+        pnToken = new JPanel();
         pnBotoes = new JPanel();
         
      //Configurando os painéis no container tela
-        tela.add(pnDados,BorderLayout.CENTER);
+        tela.add(pnDados,BorderLayout.NORTH);
+        tela.add(pnToken,BorderLayout.CENTER);
         tela.add(pnBotoes,BorderLayout.SOUTH);
         
     //Adicionando cores aos painéis
@@ -44,6 +50,8 @@ public class Janela extends JFrame{
         tfSenha = new JTextField(20);
         tfUrl = new JTextField(50);
         
+        taToken = new JTextArea("", 10, 10);
+        
         btLogin = new JButton("Login");
         
     //Adicionando os campos ao painel
@@ -58,11 +66,17 @@ public class Janela extends JFrame{
     //Adicionando os botões ao painel
         pnBotoes.setLayout(new GridLayout(1,1));
         pnBotoes.add(btLogin);
+        
     //Configurações iniciais de janela
             this.setTitle("CMJ");
             this.setSize(700, 350);
             this.setLocationRelativeTo(null);
             this.setDefaultCloseOperation(EXIT_ON_CLOSE);
             this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        
     }
 }
