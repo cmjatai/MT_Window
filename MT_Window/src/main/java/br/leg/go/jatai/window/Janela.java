bff34e40212a64b8f7285134fc0187c0f9aa311e
info <info@info-All-Series>
2024-09-04 16:01:44 -0300

    Quinta atualização: Começo da implementação do método GET

--- MT_Window/src/main/java/br/leg/go/jatai/window/Janela.java
+++ MT_Window/src/main/java/br/leg/go/jatai/window/Janela.java
@@ -15,6 +15,7 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -33,7 +34,7 @@ public class Janela extends JFrame implements ActionListener{
         private JTextField tfUser, tfUrl;
         private JPasswordField tfSenha;
         private JLabel lbUser, lbSenha, lbUrl;
-        private JButton btLogin;
+        private JButton btLogin, btGet;
         private String urlString, userString,passwordString,tokenString;
         private Container tela;
     
@@ -67,16 +68,17 @@ public class Janela extends JFrame implements ActionListener{
         
         tfUser = new JTextField(20);
         tfSenha = new JPasswordField(20);
-        tfUrl = new JTextField("https://www.jatai.go.leg.br/api/auth/token");
+        tfUrl = new JTextField(50);
         
      //Deixando impossível mexer nesse campo de texto
-        tfUrl.setEditable(false);
+        tfUrl.setEditable(true);
         
      //Definindo a área de texto com o token
-        taToken = new JTextArea(5,30);
+        taToken = new JTextArea(300,300);
         
-     //Criando o botão de login
+     //Criando o botão de login e de get
         btLogin = new JButton("Login");
+        btGet = new JButton("Get");
         
     //Adicionando os campos ao painel
         pnDados.setLayout(new GridLayout(3,2));
@@ -92,15 +94,17 @@ public class Janela extends JFrame implements ActionListener{
         pnToken.add(taToken);
     
     //Adicionando os botões ao painel
-        pnBotoes.setLayout(new GridLayout(1,1));
+        pnBotoes.setLayout(new GridLayout(1,2));
         pnBotoes.add(btLogin);
+        pnBotoes.add(btGet);
         
     //Adicionando um listener no botão
         btLogin.addActionListener(this);
+        btGet.addActionListener(this);
    
     //Configurações iniciais de janela
             this.setTitle("CMJ");
-            this.setSize(400, 150);
+            this.setSize(1000, 1000);
             this.setLocationRelativeTo(null);
             this.setDefaultCloseOperation(EXIT_ON_CLOSE);
             this.setVisible(true);
@@ -111,7 +115,7 @@ public class Janela extends JFrame implements ActionListener{
         //Adicionando um Action Listener em login
             if(e.getSource()==btLogin){
             String userString = tfUser.getText();
-            String passwordString = new String(tfSenha.getPassword());;
+            String passwordString = new String(tfSenha.getPassword());
             String urlString = tfUrl.getText();
                  try {
                         //Cria a URL e abre a conexão
@@ -151,15 +155,28 @@ public class Janela extends JFrame implements ActionListener{
                             }else{
                                 JOptionPane.showMessageDialog(this, "Erro no login: " + responseCode);
                             }
-
+                            
                        //Fecha a conexão
                             connection.disconnect();
 
                         }catch(Exception ex){
                             ex.printStackTrace();
                             JOptionPane.showMessageDialog(this,"Erro ao tentar se conectar:" +ex.getMessage());
-                        }
-
+                        }    
         }
+            
+             if(e.getSource()==btGet){
+                String userString = tfUser.getText();
+                String passwordString = tokenString;
+                String urlString = tfUrl.getText();
+                
+                try{
+                    
+
+                }catch(Exception ex){
+                    
+                }
+             }
+            
     }
 }
